d9db5b934e6f1ce106672d756b4c548fe62e4e1f
Aenewsss <aenamartinelli@gmail.com>
2025-05-18 18:19:56 -0300

    suspense added

--- app/signup/page.tsx
+++ app/signup/page.tsx
@@ -2,13 +2,13 @@
 
 import { Header } from "@/components/header";
 import { useRouter, useSearchParams } from "next/navigation";
-import { useEffect, useState } from "react";
+import { Suspense, useEffect, useState } from "react";
 import { createUserWithEmailAndPassword } from "firebase/auth"
 import { auth, db } from "@/firebase/config";
 import { Button } from "@/components/ui/button";
 import { ref, set } from "firebase/database";
 
-export default function Page() {
+function SignupForm() {
 
     const router = useRouter()
     const searchParams = useSearchParams()
@@ -56,44 +56,53 @@ export default function Page() {
     }
 
     return (
+        <form className="space-y-4 border rounded-md p-10 min-w-[400px]">
+            <div className="mb-3 flex flex-col gap-1">
+                <label htmlFor="">Nome*</label>
+                <input value={loginData.name} onChange={(e: any) => setLoginData({ ...loginData, name: e.target.value })} className="border rounded-md p-2" type="text" />
+            </div>
+            <div className="mb-3 flex flex-col gap-1">
+                <label htmlFor="">E-mail*</label>
+                <input value={loginData.email} onChange={(e: any) => setLoginData({ ...loginData, email: e.target.value })} className="border rounded-md p-2" type="email" />
+            </div>
+            <div className="mb-3 flex flex-col gap-1">
+                <label htmlFor="">Senha*</label>
+                <input value={loginData.password} onChange={(e: any) => setLoginData({ ...loginData, password: e.target.value })} className="border rounded-md p-2" type="password" />
+            </div>
+            <div className="mb-3 flex flex-col gap-1">
+                <label htmlFor="">Repetir Senha*</label>
+                <input value={loginData.repeatPassword} onChange={(e: any) => setLoginData({ ...loginData, repeatPassword: e.target.value })} className="border rounded-md p-2" type="password" />
+            </div>
+            <div className="mb-3 flex flex-col gap-1">
+                <label htmlFor="">Empresa*</label>
+                <input value={loginData.company} onChange={(e: any) => setLoginData({ ...loginData, company: e.target.value })} className="border rounded-md p-2" type="text" />
+            </div>
+            <div className="mb-3 flex flex-col gap-1">
+                <label htmlFor="">País*</label>
+                <input value={loginData.country} onChange={(e: any) => setLoginData({ ...loginData, country: e.target.value })} className="border rounded-md p-2" type="text" />
+            </div>
+            <div className="mb-3 flex flex-col gap-1">
+                <label htmlFor="">Cidade*</label>
+                <input value={loginData.city} onChange={(e: any) => setLoginData({ ...loginData, city: e.target.value })} className="border rounded-md p-2" type="text" />
+            </div>
+            <div className="mb-3 flex flex-col gap-1">
+                <label htmlFor="">Telefone*</label>
+                <input value={loginData.phone} onChange={(e: any) => setLoginData({ ...loginData, phone: e.target.value })} className="border rounded-md p-2" type="tel" />
+            </div>
+            <Button type="button" onClick={handleSignup} className="w-full" variant="default">Cadastrar</Button>
+        </form>
+    )
+}
+
+
+export default function Page() {
+    return (
         <div className="min-h-screen flex flex-col">
             <Header />
             <main className="flex-1 flex flex-col items-center justify-center px-4 py-12 md:py-24">
-                <form className="space-y-4 border rounded-md p-10 min-w-[400px]">
-                    <div className="mb-3 flex flex-col gap-1">
-                        <label htmlFor="">Nome*</label>
-                        <input value={loginData.name} onChange={(e: any) => setLoginData({ ...loginData, name: e.target.value })} className="border rounded-md p-2" type="text" />
-                    </div>
-                    <div className="mb-3 flex flex-col gap-1">
-                        <label htmlFor="">E-mail*</label>
-                        <input value={loginData.email} onChange={(e: any) => setLoginData({ ...loginData, email: e.target.value })} className="border rounded-md p-2" type="email" />
-                    </div>
-                    <div className="mb-3 flex flex-col gap-1">
-                        <label htmlFor="">Senha*</label>
-                        <input value={loginData.password} onChange={(e: any) => setLoginData({ ...loginData, password: e.target.value })} className="border rounded-md p-2" type="password" />
-                    </div>
-                    <div className="mb-3 flex flex-col gap-1">
-                        <label htmlFor="">Repetir Senha*</label>
-                        <input value={loginData.repeatPassword} onChange={(e: any) => setLoginData({ ...loginData, repeatPassword: e.target.value })} className="border rounded-md p-2" type="password" />
-                    </div>
-                    <div className="mb-3 flex flex-col gap-1">
-                        <label htmlFor="">Empresa*</label>
-                        <input value={loginData.company} onChange={(e: any) => setLoginData({ ...loginData, company: e.target.value })} className="border rounded-md p-2" type="text" />
-                    </div>
-                    <div className="mb-3 flex flex-col gap-1">
-                        <label htmlFor="">País*</label>
-                        <input value={loginData.country} onChange={(e: any) => setLoginData({ ...loginData, country: e.target.value })} className="border rounded-md p-2" type="text" />
-                    </div>
-                    <div className="mb-3 flex flex-col gap-1">
-                        <label htmlFor="">Cidade*</label>
-                        <input value={loginData.city} onChange={(e: any) => setLoginData({ ...loginData, city: e.target.value })} className="border rounded-md p-2" type="text" />
-                    </div>
-                    <div className="mb-3 flex flex-col gap-1">
-                        <label htmlFor="">Telefone*</label>
-                        <input value={loginData.phone} onChange={(e: any) => setLoginData({ ...loginData, phone: e.target.value })} className="border rounded-md p-2" type="tel" />
-                    </div>
-                    <Button type="button" onClick={handleSignup} className="w-full" variant="default">Cadastrar</Button>
-                </form>
+                <Suspense fallback={<p>Carregando...</p>}>
+                    <SignupForm />
+                </Suspense>
             </main>
             <footer className="border-t py-6 md:py-8  flex items-center justify-center gap-4 md:flex-row md:gap-8 ">
                 <p className="text-center text-sm text-muted-foreground">© 2025 DocGen. Todos os direitos reservados.
@@ -101,5 +110,5 @@ export default function Page() {
                 </p>
             </footer>
         </div>
-    )
+    );
 }
